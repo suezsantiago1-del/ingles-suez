e64573f55efff3bff7aa75b3629a8179c768c030
Santiago Suez <suezsantiago1@gmail.com>
2026-08-21 02:01:28 -0300

    x

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -877,7 +877,7 @@ export const descargarCertificado = async (req, res) => {
 
         // Mes (Alineado tras "del mes de")
         firstPage.drawText(mes, {
-            x: 465,
+            x: 460,
             y: 172,
             size: 11,
             font: fontBold,
